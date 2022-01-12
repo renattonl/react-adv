db003712c7ad780069453389fe9b6bb78dad0220
RenattoNL <renattonl@gmail.com>
2022-01-12 12:42:27 -0500

    Control props alternativo

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { onChangeArgs, Product } from '../interfaces/interfaces';
 
 interface useProductArgs{
@@ -10,12 +10,7 @@ interface useProductArgs{
 export const useProduct = ({onChange, product, value = 0}: useProductArgs ) => {
     const [counter, setCounter] = useState( value )
 
-    const isController = useRef( !!onChange )
-
     const increaseBy = (val : number) => {
-      if(isController.current){
-        return onChange!({count: val, product})
-      }
       const newValue = Math.max(counter + val, 0)
       setCounter(newValue)
       onChange && onChange({count: newValue, product});
--- src/02-component-patterns/hooks/useShoppingCart.tsx
+++ src/02-component-patterns/hooks/useShoppingCart.tsx
@@ -5,24 +5,15 @@ export const useShoppingCart = () => {
 
   const [shoppingCart, setShoppingCart] = useState<{[key: string]: ProductInCart}>({})
   const onProductCountChange = ({count, product}: {count: number, product: Product}) => {
-    // console.log({count});
-
     setShoppingCart(e => {
-      const productInCart: ProductInCart = e[product.id] ?? {...product, count: 0}
-      if(Math.max(productInCart.count + count, 0) > 0){
-        productInCart.count += count;
-        return {...e, [product.id] : productInCart}
+      if(!count) {
+        const { [product.id]: toDelete, ...ee } = e ?? {};
+        return ee;
       }
-      const {[product.id]: toDelete, ...rest} = e
-      return rest;
-      // if(!count) {
-      //   const { [product.id]: toDelete, ...ee } = e ?? {};
-      //   return ee;
-      // }
-      // return {
-      //   ...e,
-      //   [product.id]: {...product, count},
-      // }
+      return {
+        ...e,
+        [product.id]: {...product, count},
+      }
     })
   };
   
